e5bb74afc5a9604c5cfa4b525a46566ae4b03951
pchiranjivi2050 <123458290+pchiranjivi2050@users.noreply.github.com>
2025-06-09 21:13:36 +0545

    new

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: [
+    'https://fe-e7v373qzc-chiranjivi-poudels-projects.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
